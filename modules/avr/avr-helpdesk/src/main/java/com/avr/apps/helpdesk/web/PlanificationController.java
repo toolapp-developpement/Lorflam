1a7107d3c6045a8b80185da753232746240cb5d6
Timmy44119 <v.gautier@toolapp.fr>
2022-04-13 16:23:41 +0200

    Corection import methode prepared

--- modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/web/PlanificationController.java
+++ modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/web/PlanificationController.java
@@ -1,14 +1,12 @@
 package com.avr.apps.helpdesk.web;
 
-import org.apache.xmlbeans.impl.xb.xsdschema.Public;
-import org.camunda.bpm.model.dmn.instance.Context;
-
 import com.axelor.apps.stock.db.StockMove;
 import com.axelor.apps.stock.db.StockMoveLine;
 import com.axelor.apps.stock.db.repo.StockMoveRepository;
 import com.axelor.inject.Beans;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
+import com.axelor.exception.AxelorException;
 
 public class PlanificationController {
 
