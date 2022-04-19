fdf3c12944c8156fc656723d466e14201dc6aed4
Timmy44119 <v.gautier@toolapp.fr>
2022-04-19 17:56:22 +0200

    Correction modification

--- modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/modules/TicketModule.java
+++ modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/modules/TicketModule.java
@@ -1,16 +1,12 @@
 package com.avr.apps.helpdesk.modules;
 
-import com.axelor.apps.production.db.repo.StockMoveLineProductionRepository;
-import com.avr.apps.helpdesk.repository.StockMoveLineAvrHelpdeskRepository;
 import com.avr.apps.helpdesk.service.TicketService;
 import com.avr.apps.helpdesk.service.impl.TicketServiceImpl;
 import com.axelor.app.AxelorModule;
 
-
 public class TicketModule extends AxelorModule {
-  @Override
-  protected void configure() {
-    bind(TicketService.class).to(TicketServiceImpl.class);
-    bind(StockMoveLineProductionRepository.class).to(StockMoveLineAvrHelpdeskRepository.class);
-  }
+	@Override
+	protected void configure() {
+		bind(TicketService.class).to(TicketServiceImpl.class);
+	}
 }
