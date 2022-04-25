107cde1a6b875126b3e7ce65e76fdb82369fc099
David Barbarin <d.barbarin@avr-info.com>
2022-04-25 12:20:35 +0200

    feat(DI): inject all class modified

--- modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/modules/TicketModule.java
+++ modules/avr/avr-helpdesk/src/main/java/com/avr/apps/helpdesk/modules/TicketModule.java
@@ -1,15 +1,22 @@
 package com.avr.apps.helpdesk.modules;
 
-import com.avr.apps.helpdesk.service.PurchaseOrderCreationWithYardService;
+import com.avr.apps.helpdesk.service.PurchaseOrderCreationService;
+import com.avr.apps.helpdesk.service.StockMoveCreateService;
 import com.avr.apps.helpdesk.service.TicketService;
-import com.avr.apps.helpdesk.service.impl.PurchaseOrderCreateSupplychainServiceImpl;
-import com.avr.apps.helpdesk.service.impl.TicketServiceImpl;
+import com.avr.apps.helpdesk.service.impl.*;
 import com.axelor.app.AxelorModule;
+import com.axelor.apps.supplychain.service.PurchaseOrderStockServiceImpl;
+import com.axelor.apps.supplychain.service.SaleOrderPurchaseServiceImpl;
+import com.axelor.apps.supplychain.service.SaleOrderStockServiceImpl;
 
 public class TicketModule extends AxelorModule {
     @Override
     protected void configure(){
         bind(TicketService.class).to(TicketServiceImpl.class);
-        bind(PurchaseOrderCreationWithYardService.class).to(PurchaseOrderCreateSupplychainServiceImpl.class);
+        bind(PurchaseOrderCreationService.class).to(PurchaseOrderCreateSupplychainServiceImpl.class);
+        bind(StockMoveCreateService.class).to(StockMoveCreateServiceImpl.class);
+        bind(SaleOrderStockServiceImpl.class).to(SaleOrderCreateStockMoveServiceImpl.class);
+        bind(SaleOrderPurchaseServiceImpl.class).to(SaleOrderCreatePurchaseServiceImpl.class);
+        bind(PurchaseOrderStockServiceImpl.class).to(PurchaseOrderCreateStockServiceImpl.class);
     }
 }
