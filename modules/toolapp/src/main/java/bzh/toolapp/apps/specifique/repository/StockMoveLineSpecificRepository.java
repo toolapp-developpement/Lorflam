deac2f24ae3ea6b147af72c6a2f152a50c1dca6b
Timmy44119 <v.gautier@toolapp.fr>
2022-04-28 14:40:12 +0200

    Mise à jour Champs Coco

--- modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/repository/StockMoveLineSpecificRepository.java
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/repository/StockMoveLineSpecificRepository.java
@@ -8,12 +8,13 @@ import org.slf4j.LoggerFactory;
 
 import com.axelor.apps.production.db.repo.StockMoveLineProductionRepository;
 import com.axelor.apps.sale.db.SaleOrder;
-import com.axelor.apps.stock.db.StockMove;
 import com.axelor.apps.stock.db.StockMoveLine;
+import com.google.inject.persist.Transactional;
 
 public class StockMoveLineSpecificRepository extends StockMoveLineProductionRepository {
 	private final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+	@Transactional
 	@Override
 	public Map<String, Object> populate(Map<String, Object> json, Map<String, Object> context) {
 		// Recuperation du champ de lecran
@@ -26,27 +27,31 @@ public class StockMoveLineSpecificRepository extends StockMoveLineProductionRepo
 		StockMoveLine sml = null;
 		if (id != null) {
 			sml = find(id);
-			if (sml.getSaleOrderLine() != null && sml.getSaleOrderLine().getSaleOrder() != null) {
-				saleOrderSeq = sml.getSaleOrderLine().getSaleOrder().getSaleOrderSeq();
-			} else if (sml.getProducedManufOrder() != null && sml.getProducedManufOrder().getSaleOrderSet() != null) {
-				if (!sml.getProducedManufOrder().getSaleOrderSet().isEmpty()) {
-					SaleOrder so = sml.getProducedManufOrder().getSaleOrderSet().iterator().next();
-					saleOrderSeq = so.getSaleOrderSeq();
+			if (sml.getCustSaleOrderSeq() != null) {
+
+				if (sml.getSaleOrderLine() != null && sml.getSaleOrderLine().getSaleOrder() != null) {
+					saleOrderSeq = sml.getSaleOrderLine().getSaleOrder().getSaleOrderSeq();
+				} else if (sml.getProducedManufOrder() != null
+						&& sml.getProducedManufOrder().getSaleOrderSet() != null) {
+					if (!sml.getProducedManufOrder().getSaleOrderSet().isEmpty()) {
+						SaleOrder so = sml.getProducedManufOrder().getSaleOrderSet().iterator().next();
+						saleOrderSeq = so.getSaleOrderSeq();
+					}
+				} else if (sml.getConsumedManufOrder() != null
+						&& sml.getConsumedManufOrder().getSaleOrderSet() != null) {
+					if (!sml.getConsumedManufOrder().getSaleOrderSet().isEmpty()) {
+						SaleOrder so = sml.getConsumedManufOrder().getSaleOrderSet().iterator().next();
+						saleOrderSeq = so.getSaleOrderSeq();
+					}
 				}
-			} else if (sml.getConsumedManufOrder() != null && sml.getConsumedManufOrder().getSaleOrderSet() != null) {
-				if (!sml.getConsumedManufOrder().getSaleOrderSet().isEmpty()) {
-					SaleOrder so = sml.getConsumedManufOrder().getSaleOrderSet().iterator().next();
-					saleOrderSeq = so.getSaleOrderSeq();
-				}
+
+				sml.setCustSaleOrderSeq(saleOrderSeq);
+				this.save(sml);
+
+				this.logger.debug("L'origine est {}", saleOrderSeq);
+
+				json.put("stockMoveLine.custSaleOrderSeq", saleOrderSeq);
 			}
-
-			StockMove sm = sml.getStockMove();
-
-			sm.setCustSaleOrderSeq(saleOrderSeq);
-
-			this.logger.debug("L'origine est {}", saleOrderSeq);
-
-			json.put("custSaleOrderSeq", saleOrderSeq);
 		}
 
 		return super.populate(json, context);
