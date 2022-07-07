86f0272408c5e9a2ae7cd92639f06a7cd12629f7
Timmy44119 <v.gautier@toolapp.fr>
2022-07-07 10:20:09 +0200

    Modification du champ Tarif

---
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/SaleOrderLineSpecifiqueServiceImpl.java
@@ -0,0 +1,44 @@
+package bzh.toolapp.apps.specifique.service.impl;
+
+import java.math.BigDecimal;
+
+import com.axelor.apps.account.service.AnalyticMoveLineService;
+import com.axelor.apps.account.service.app.AppAccountService;
+import com.axelor.apps.base.service.CurrencyService;
+import com.axelor.apps.base.service.PriceListService;
+import com.axelor.apps.base.service.ProductMultipleQtyService;
+import com.axelor.apps.base.service.app.AppBaseService;
+import com.axelor.apps.base.service.tax.AccountManagementService;
+import com.axelor.apps.businessproduction.service.SaleOrderLineBusinessProductionServiceImpl;
+import com.axelor.apps.sale.db.PackLine;
+import com.axelor.apps.sale.db.SaleOrder;
+import com.axelor.apps.sale.db.SaleOrderLine;
+import com.axelor.apps.sale.db.repo.SaleOrderLineRepository;
+import com.axelor.apps.sale.service.app.AppSaleService;
+import com.axelor.apps.supplychain.service.app.AppSupplychainService;
+import com.google.inject.Inject;
+
+public class SaleOrderLineSpecifiqueServiceImpl extends SaleOrderLineBusinessProductionServiceImpl {
+	@Inject
+	public SaleOrderLineSpecifiqueServiceImpl(CurrencyService currencyService, PriceListService priceListService,
+			ProductMultipleQtyService productMultipleQtyService, AppBaseService appBaseService,
+			AppSaleService appSaleService, AccountManagementService accountManagementService,
+			SaleOrderLineRepository saleOrderLineRepo, AppAccountService appAccountService,
+			AnalyticMoveLineService analyticMoveLineService, AppSupplychainService appSupplychainService) {
+		super(currencyService, priceListService, productMultipleQtyService, appBaseService, appSaleService,
+				accountManagementService, saleOrderLineRepo, appAccountService, analyticMoveLineService,
+				appSupplychainService);
+	}
+
+	@Override
+	public SaleOrderLine createSaleOrderLine(PackLine packLine, SaleOrder saleOrder, BigDecimal packQty,
+			BigDecimal conversionRate, Integer sequence) {
+
+		SaleOrderLine soLine = super.createSaleOrderLine(packLine, saleOrder, packQty, conversionRate, sequence);
+
+		soLine.setRateToApplySol(saleOrder.getTicket().getRateToApply().getName());
+
+		return soLine;
+	}
+
+}
