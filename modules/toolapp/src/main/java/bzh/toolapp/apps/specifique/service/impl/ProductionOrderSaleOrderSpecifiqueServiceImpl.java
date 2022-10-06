fe5314a09f5c4a287bb9143a4fb1a08cdbfb7d4b
Timmy44119 <v.gautier@toolapp.fr>
2022-10-06 15:47:18 +0200

    MA1-T26 : Ajout d'un bouton modifier commande : Test Gestion de la supression des postes

--- modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/ProductionOrderSaleOrderSpecifiqueServiceImpl.java
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/ProductionOrderSaleOrderSpecifiqueServiceImpl.java
@@ -17,7 +17,6 @@ import com.axelor.exception.AxelorException;
 import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.i18n.I18n;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.inject.Inject;
 
@@ -93,7 +92,7 @@ public class ProductionOrderSaleOrderSpecifiqueServiceImpl
        */
       LocalDateTime startedDate = LocalDateTime.now();
       if (saleOrderLine.getSaleOrder().getDeliveryDate() != null) {
-        LocalDate startDate = saleOrderLine.getSaleOrder().getDeliveryDate();
+        startedDate = saleOrderLine.getSaleOrder().getDeliveryDate().atStartOfDay();
         /* startedDate = startDate.minusDays(3).atStartOfDay(); */
       }
 
