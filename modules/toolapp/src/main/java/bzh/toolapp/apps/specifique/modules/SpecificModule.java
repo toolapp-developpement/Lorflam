697bea0a06669562bf1e8564d8101fe42d2a2142
Timmy44119 <v.gautier@toolapp.fr>
2022-10-14 17:48:58 +0200

    MA1-T35 : Modification commande de vente annulation OF/OP

--- modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/modules/SpecificModule.java
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/modules/SpecificModule.java
@@ -1,33 +1,37 @@
 package bzh.toolapp.apps.specifique.modules;
 
-import com.avr.apps.helpdesk.service.impl.PurchaseOrderCreateStockServiceImpl;
-import com.avr.apps.helpdesk.service.impl.SaleOrderCreateStockMoveServiceImpl;
-import com.axelor.app.AxelorModule;
-import com.axelor.apps.businessproduction.service.ProductionOrderSaleOrderServiceBusinessImpl;
-import com.axelor.apps.production.db.repo.StockMoveLineProductionRepository;
-
 import bzh.toolapp.apps.specifique.repository.StockMoveLineSpecificRepository;
 import bzh.toolapp.apps.specifique.service.SpecifiqueService;
 import bzh.toolapp.apps.specifique.service.StockMoveLineSpecifiqueCreationService;
 import bzh.toolapp.apps.specifique.service.impl.*;
-import com.avr.apps.helpdesk.service.impl.PurchaseOrderCreateSupplychainServiceImpl;
 import bzh.toolapp.apps.specifique.service.impl.ProductionOrderSaleOrderSpecifiqueServiceImpl;
 import bzh.toolapp.apps.specifique.service.impl.PurchaseOrderStockSpecifiqueServiceImpl;
 import bzh.toolapp.apps.specifique.service.impl.SaleOrderStockSpecifiqueServiceImpl;
 import bzh.toolapp.apps.specifique.service.impl.SpecifiqueServiceImpl;
 import bzh.toolapp.apps.specifique.service.impl.StockMoveLineSpecifiqueCreationServiceImpl;
+import com.avr.apps.helpdesk.service.impl.PurchaseOrderCreateStockServiceImpl;
+import com.avr.apps.helpdesk.service.impl.PurchaseOrderCreateSupplychainServiceImpl;
+import com.avr.apps.helpdesk.service.impl.SaleOrderCreateStockMoveServiceImpl;
+import com.axelor.app.AxelorModule;
+import com.axelor.apps.businessproduction.service.ProductionOrderSaleOrderServiceBusinessImpl;
+import com.axelor.apps.production.db.repo.StockMoveLineProductionRepository;
+import com.axelor.apps.supplychain.service.SaleOrderServiceSupplychainImpl;
 
 public class SpecificModule extends AxelorModule {
 
-    @Override
-    protected void configure() {
-        bind(StockMoveLineProductionRepository.class).to(StockMoveLineSpecificRepository.class);
-        bind(StockMoveLineSpecifiqueCreationService.class).to(StockMoveLineSpecifiqueCreationServiceImpl.class);
-        bind(SaleOrderCreateStockMoveServiceImpl.class).to(SaleOrderStockSpecifiqueServiceImpl.class);
-        bind(PurchaseOrderCreateStockServiceImpl.class).to(PurchaseOrderStockSpecifiqueServiceImpl.class);
-        bind(SpecifiqueService.class).to(SpecifiqueServiceImpl.class);
-        bind(ProductionOrderSaleOrderServiceBusinessImpl.class).to(ProductionOrderSaleOrderSpecifiqueServiceImpl.class);
-        bind(PurchaseOrderCreateSupplychainServiceImpl.class)
-                .to(PurchaseOrderCreateSpecifiqueServiceImpl.class);
-    }
+  @Override
+  protected void configure() {
+    bind(StockMoveLineProductionRepository.class).to(StockMoveLineSpecificRepository.class);
+    bind(StockMoveLineSpecifiqueCreationService.class)
+        .to(StockMoveLineSpecifiqueCreationServiceImpl.class);
+    bind(SaleOrderCreateStockMoveServiceImpl.class).to(SaleOrderStockSpecifiqueServiceImpl.class);
+    bind(PurchaseOrderCreateStockServiceImpl.class)
+        .to(PurchaseOrderStockSpecifiqueServiceImpl.class);
+    bind(SpecifiqueService.class).to(SpecifiqueServiceImpl.class);
+    bind(ProductionOrderSaleOrderServiceBusinessImpl.class)
+        .to(ProductionOrderSaleOrderSpecifiqueServiceImpl.class);
+    bind(PurchaseOrderCreateSupplychainServiceImpl.class)
+        .to(PurchaseOrderCreateSpecifiqueServiceImpl.class);
+    bind(SaleOrderServiceSupplychainImpl.class).to(SaleOrderServiceSpecifiqueImpl.class);
+  }
 }
