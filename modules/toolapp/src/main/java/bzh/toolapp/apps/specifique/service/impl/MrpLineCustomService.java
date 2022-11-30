10c5b22ec56d4ea6ecfae4b54a5e85125a7e14fe
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-11-30 12:56:06 +0000

    écran CBN

--- modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/MrpLineCustomService.java
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/MrpLineCustomService.java
@@ -2,6 +2,8 @@ package bzh.toolapp.apps.specifique.service.impl;
 
 import com.axelor.apps.base.db.Product;
 import com.axelor.apps.base.db.repo.ProductRepository;
+import com.axelor.apps.production.db.BillOfMaterial;
+import com.axelor.apps.production.exceptions.IExceptionMessage;
 import com.axelor.apps.stock.db.StockLocation;
 import com.axelor.apps.stock.db.StockRules;
 import com.axelor.apps.stock.db.repo.StockConfigRepository;
@@ -10,6 +12,7 @@ import com.axelor.apps.stock.db.repo.StockMoveLineRepository;
 import com.axelor.apps.stock.db.repo.StockRulesRepository;
 import com.axelor.apps.stock.service.StockRulesService;
 import com.axelor.apps.stock.service.config.StockConfigService;
+import com.axelor.apps.supplychain.db.Mrp;
 import com.axelor.apps.supplychain.db.MrpLine;
 import com.axelor.apps.supplychain.db.repo.MrpLineRepository;
 import com.axelor.apps.supplychain.db.repo.MrpLineTypeRepository;
@@ -18,6 +21,9 @@ import com.axelor.apps.toolapp.db.MrpLineCustom;
 import com.axelor.auth.AuthUtils;
 import com.axelor.db.JPA;
 import com.axelor.exception.AxelorException;
+import com.axelor.exception.db.repo.TraceBackRepository;
+import com.axelor.i18n.I18n;
+import com.google.common.collect.Maps;
 import com.google.inject.Inject;
 import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
@@ -28,6 +34,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Map.Entry;
 import javax.persistence.Query;
 import org.slf4j.Logger;
@@ -44,6 +51,8 @@ public class MrpLineCustomService {
   StockConfigRepository stockConfigRepository;
   StockConfigService stockConfigService;
   MrpRepository mrpRepository;
+  Map<Long, Integer> productMap;
+  Map<Long, List<BigDecimal>> productQtyMap;
 
   @Inject
   public MrpLineCustomService(
@@ -63,10 +72,12 @@ public class MrpLineCustomService {
     this.stockConfigRepository = stockConfigRepository;
     this.stockConfigService = stockConfigService;
     this.mrpRepository = mrpRepository;
+    this.productMap = Maps.newHashMap();
+    this.productQtyMap = Maps.newHashMap();
   }
 
   public List<MrpLineCustom> computeMrpLineCustomList(
-      Long productId, Long stockLocationId, LocalDate date) {
+      Long productId, Long stockLocationId, LocalDate date) throws AxelorException {
     List<MrpLineCustom> mrpLineCustomList = new ArrayList<>();
     Product product = productRepository.find(productId);
     StockLocation stockLocation = stockLocationRepository.find(stockLocationId);
@@ -93,20 +104,20 @@ public class MrpLineCustomService {
                 product,
                 date,
                 stockLocation)
-            .order("-id")
+            .order("-mrp.endDateTime")
             .fetchOne();
     if (lastMrpLine == null) {
       return mrpLineCustomList;
     }
     LOG.debug("Mrp : {}", lastMrpLine.getMrp());
+    this.assignProductAndLevel(product, lastMrpLine.getMrp());
     // on parcours les ligne de CBN
     for (MrpLine mrpLine :
         mrpLineRepository
             .all()
             .filter(
-                "self.product = ?1 AND self.mrp.endDate <= ?2 AND self.mrp = ?3 AND self.mrpLineType.elementSelect IN "
-                    + filtreType,
-                product,
+                "self.product.id IN (?1) AND self.mrp.endDate <= ?2 AND self.mrp = ?3 ",
+                this.productMap.keySet(),
                 date,
                 lastMrpLine.getMrp())
             .order("maturityDate")
@@ -114,21 +125,37 @@ public class MrpLineCustomService {
             .order("mrpLineType.sequence")
             .order("id")
             .fetch()) {
+      // si les totaux existent déjà dans la map, on va les chercher
+      Long productIdMrpLine = mrpLine.getProduct().getId();
+      if (!productQtyMap.containsKey(productIdMrpLine)) {
+        productQtyMap.put(productIdMrpLine, this.getValueQty(productIdMrpLine, stockLocationId));
+      }
+      List<BigDecimal> valueList;
+      valueList = productQtyMap.get(mrpLine.getProduct().getId());
+      BigDecimal minQty = valueList.get(0);
+      BigDecimal n2 = valueList.get(1);
+      BigDecimal n1 = valueList.get(2);
+      BigDecimal n = valueList.get(3);
+
       // get week number from MaturityDate
       Integer weekNumber = getWeekNumber(mrpLine.getMaturityDate());
-      String key = "" + weekNumber + mrpLine.getMrpLineType().getId();
-
+      String key =
+          "" + weekNumber + mrpLine.getMrpLineType().getId() + mrpLine.getProduct().getId();
+      // si la ligne n'existe pas pour l'article la semaine et le type, alors on la crée
       if (!map.containsKey(key)) {
         MrpLineCustom mrpLineCustom = new MrpLineCustom();
-        mrpLineCustom.setProduct(product);
-        mrpLineCustom.setStockLocation(stockLocation);
+        mrpLineCustom.setProduct(mrpLine.getProduct());
+        mrpLineCustom.setStockLocation(mrpLine.getStockLocation());
         mrpLineCustom.setMrp(mrpLine.getMrp());
         mrpLineCustom.setMrpLineType(mrpLine.getMrpLineType());
         mrpLineCustom.setWeekNumber(weekNumber);
         mrpLineCustom.setStockLocation(stockLocation);
         mrpLineCustom.setQty(mrpLine.getQty());
         mrpLineCustom.setCumulativeQty(mrpLine.getCumulativeQty());
-        mrpLineCustom.setMinQty(mrpLine.getMinQty());
+        mrpLineCustom.setMinQty(minQty);
+        mrpLineCustom.setQtyN2(n2);
+        mrpLineCustom.setQtyN1(n1);
+        mrpLineCustom.setQtyN(n);
         mrpLineCustom.setIdealQty(mrpLine.getIdealQty());
         mrpLineCustom.setReOrderQty(mrpLine.getReOrderQty());
         mrpLineCustom.setMaturityDate(mrpLine.getMaturityDate());
@@ -139,9 +166,11 @@ public class MrpLineCustomService {
         mrpLineCustom.setProposalSelectId(mrpLine.getProposalSelectId());
         mrpLineCustom.setProposalToProcess(mrpLine.getProposalToProcess());
         mrpLineCustom.setRelatedToSelectName(mrpLine.getRelatedToSelectName());
-        mrpLineCustom.setMaxLevel(mrpLine.getMaxLevel());
+        mrpLineCustom.setMaxLevel(this.getMaxLevel(mrpLine.getProduct(), mrpLine.getMaxLevel()));
         map.put(key, mrpLineCustom);
-      } else {
+      }
+      // sinon on mets à jour les quantités
+      else {
         MrpLineCustom mrpLineCustom = (MrpLineCustom) map.get(key);
         mrpLineCustom.setQty(mrpLineCustom.getQty().add(mrpLine.getQty()));
         mrpLineCustom.setCumulativeQty(mrpLine.getCumulativeQty());
@@ -258,5 +287,82 @@ public class MrpLineCustomService {
     return (result.get(0) == null || ((BigDecimal) result.get(0)).signum() == 0)
         ? BigDecimal.ZERO
         : ((BigDecimal) result.get(0)).setScale(2, BigDecimal.ROUND_HALF_UP);
+  }
+
+  protected void assignProductAndLevel(Product product, Mrp mrp) throws AxelorException {
+
+    if (product.getDefaultBillOfMaterial() != null
+        && mrp.getMrpTypeSelect() == MrpRepository.MRP_TYPE_MRP) {
+      this.assignProductLevel(product.getDefaultBillOfMaterial(), 0);
+    } else {
+      LOG.debug("Add product: {}", product.getFullName());
+      this.productMap.put(product.getId(), this.getMaxLevel(product, 0));
+    }
+  }
+
+  /**
+   * Update the level of Bill of materials. The highest for each product (0: product with parent, 1:
+   * product with a parent, 2: product with a parent that have a parent, ...)
+   *
+   * @param billOfMaterial
+   * @param level
+   */
+  protected void assignProductLevel(BillOfMaterial billOfMaterial, int level)
+      throws AxelorException {
+
+    if (level > 100) {
+      if (billOfMaterial == null || billOfMaterial.getProduct() == null) {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+            I18n.get(IExceptionMessage.MRP_BOM_LEVEL_TOO_HIGH));
+      } else {
+        throw new AxelorException(
+            TraceBackRepository.CATEGORY_CONFIGURATION_ERROR,
+            I18n.get(IExceptionMessage.MRP_BOM_LEVEL_TOO_HIGH_PRODUCT),
+            billOfMaterial.getProduct().getFullName());
+      }
+    }
+
+    Product product = billOfMaterial.getProduct();
+    this.productMap.put(product.getId(), this.getMaxLevel(product, level));
+
+    level = level + 1;
+    if (billOfMaterial.getBillOfMaterialSet() != null
+        && !billOfMaterial.getBillOfMaterialSet().isEmpty()) {
+
+      for (BillOfMaterial subBillOfMaterial : billOfMaterial.getBillOfMaterialSet()) {
+
+        Product subProduct = subBillOfMaterial.getProduct();
+
+        if (this.isMrpProduct(subProduct)) {
+          this.assignProductLevel(subBillOfMaterial, level);
+
+          if (subProduct.getDefaultBillOfMaterial() != null) {
+            this.assignProductLevel(subProduct.getDefaultBillOfMaterial(), level);
+          }
+        }
+      }
+    }
+  }
+
+  public int getMaxLevel(Product product, int level) {
+
+    if (this.productMap.containsKey(product.getId())) {
+      return Math.max(level, this.productMap.get(product.getId()));
+    }
+
+    return level;
+  }
+
+  public boolean isMrpProduct(Product product) {
+
+    if (product != null
+        && !product.getExcludeFromMrp()
+        && product.getProductTypeSelect().equals(ProductRepository.PRODUCT_TYPE_STORABLE)) {
+
+      return true;
+    }
+
+    return false;
   }
 }
---
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/web/ProductController.java
@@ -0,0 +1,133 @@
+package bzh.toolapp.apps.specifique.web;
+
+// import bzh.toolapp.apps.specifique.service.etatstock.BillOfMaterialServiceSpecifique;
+import com.axelor.apps.supplychain.db.MrpLine;
+import com.axelor.apps.supplychain.service.ProjectedStockService;
+import com.axelor.apps.toolapp.db.MrpLineCustom;
+import com.axelor.db.mapper.Mapper;
+import com.axelor.exception.service.TraceBackService;
+import com.axelor.i18n.I18n;
+import com.axelor.inject.Beans;
+import com.axelor.meta.schema.actions.ActionView;
+import com.axelor.rpc.ActionRequest;
+import com.axelor.rpc.ActionResponse;
+import com.axelor.rpc.Context;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ProductController {
+  //   public void openProductTree(ActionRequest request, ActionResponse response)
+  //       throws AxelorException {
+
+  //     TempBomTree bomTree = request.getContext().asType(TempBomTree.class);
+  //     Product product = bomTree.getProduct();
+  //     Company company = bomTree.getCompany();
+  //     StockLocation stockLocation = bomTree.getStockLocation();
+
+  //     TempBomTree tempBomTree =
+  //         Beans.get(BillOfMaterialServiceSpecifique.class)
+  //             .generateTree(
+  //                 product.getDefaultBillOfMaterial(), true, stockLocation.getId(),
+  // company.getId());
+
+  //     response.setView(
+  //         ActionView.define(I18n.get("Bill of materials"))
+  //             .model(TempBomTree.class.getName())
+  //             .add("tree", "bom-tree-detail")
+  //             .context("_tempBomTreeId", tempBomTree.getId())
+  //             .map());
+  //   }
+
+  public void showProjectedStock(ActionRequest request, ActionResponse response) {
+
+    try {
+      ProjectedStockService projectedStockService = Beans.get(ProjectedStockService.class);
+
+      MrpLineCustom mrpLine = request.getContext().asType(MrpLineCustom.class);
+
+      Map<String, Long> mapId = new HashMap<>();
+
+      mapId.put("productId", mrpLine.getProduct().getId());
+      mapId.put("companyId", mrpLine.getStockLocation().getCompany().getId());
+      mapId.put("stockLocationId", mrpLine.getStockLocation().getId());
+
+      final List<MrpLine> mrpLineList = new ArrayList<>();
+      try {
+        mrpLineList.addAll(
+            projectedStockService.createProjectedStock(
+                mapId.get("productId"), mapId.get("companyId"), mapId.get("stockLocationId")));
+        response.setView(
+            ActionView.define(I18n.get("Projected stock"))
+                .model(MrpLine.class.getName())
+                .add("form", "projected-stock-form-custome")
+                .param("popup", "true")
+                .param("popup-save", "false")
+                .param("popup.maximized", "true")
+                .context("_mrpLineList", mrpLineList)
+                .map());
+      } catch (Exception e) {
+        TraceBackService.trace(response, e);
+      } finally {
+        projectedStockService.removeMrpAndMrpLine(mrpLineList);
+      }
+    } catch (Exception e) {
+      TraceBackService.trace(response, e);
+    }
+  }
+
+  public void showCustomeChartProjectedStock(ActionRequest request, ActionResponse response) {
+    Context context = request.getContext();
+    List<Map<String, Object>> dataList = new ArrayList<>();
+
+    @SuppressWarnings("unchecked")
+    Collection<Map<String, Object>> contextMrpLineList =
+        (Collection<Map<String, Object>>) context.get("_mrpLineListToProject");
+
+    List<MrpLine> mrpLineList =
+        contextMrpLineList.stream()
+            .map(map -> Mapper.toBean(MrpLine.class, map))
+            .collect(Collectors.toList());
+
+    if (!mrpLineList.isEmpty()) {
+      List<MrpLine> mrpLineLastList = new ArrayList<>();
+      MrpLine lastMrpLine = mrpLineList.get(0);
+
+      for (int i = 1; i < mrpLineList.size(); ++i) {
+        MrpLine mrpLine = mrpLineList.get(i);
+        if (mrpLine.getMaturityDate().isAfter(lastMrpLine.getMaturityDate())) {
+          mrpLineLastList.add(lastMrpLine);
+        }
+        lastMrpLine = mrpLine;
+      }
+      mrpLineLastList.add(lastMrpLine);
+      lastMrpLine = mrpLineList.get(0);
+      LocalDate mrpDate = lastMrpLine.getMaturityDate();
+      for (MrpLine mrpLine : mrpLineLastList) {
+        mrpDate = addInterElementForProjectedStockChart(dataList, lastMrpLine, mrpDate, mrpLine);
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("name", mrpLine.getMaturityDate());
+        dataMap.put("cumulativeQty", mrpLine.getCumulativeQty());
+        dataList.add(dataMap);
+        lastMrpLine = mrpLine;
+      }
+    }
+    response.setData(dataList);
+  }
+
+  private LocalDate addInterElementForProjectedStockChart(
+      List<Map<String, Object>> dataList, MrpLine lastMrpLine, LocalDate mrpDate, MrpLine mrpLine) {
+    while (mrpDate.isBefore(mrpLine.getMaturityDate())) {
+      mrpDate = mrpDate.plusDays(1);
+      Map<String, Object> dataMapDate = new HashMap<>();
+      dataMapDate.put("name", mrpDate);
+      dataMapDate.put("cumulativeQty", lastMrpLine.getCumulativeQty());
+      dataList.add(dataMapDate);
+    }
+    return mrpDate;
+  }
+}
