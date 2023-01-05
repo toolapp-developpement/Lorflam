dea597b41b6dc9c97883f07dda0d0d96b9814149
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2023-01-05 10:32:58 +0000

    Apply spotless

--- modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/StockMoveInvoiceServiceImplSpecifique.java
+++ modules/toolapp/src/main/java/bzh/toolapp/apps/specifique/service/impl/StockMoveInvoiceServiceImplSpecifique.java
@@ -5,12 +5,10 @@ import com.axelor.apps.account.db.InvoiceLine;
 import com.axelor.apps.account.db.repo.InvoiceLineRepository;
 import com.axelor.apps.account.db.repo.InvoiceRepository;
 import com.axelor.apps.account.service.invoice.InvoiceLineService;
-import com.axelor.apps.account.service.invoice.InvoiceService;
 import com.axelor.apps.account.service.invoice.generator.line.InvoiceLineManagement;
 import com.axelor.apps.businessproject.service.ProjectStockMoveInvoiceServiceImpl;
 import com.axelor.apps.purchase.db.repo.PurchaseOrderRepository;
 import com.axelor.apps.sale.db.repo.SaleOrderRepository;
-import com.axelor.apps.stock.db.StockMove;
 import com.axelor.apps.stock.db.StockMoveLine;
 import com.axelor.apps.stock.db.repo.StockMoveLineRepository;
 import com.axelor.apps.supplychain.service.PurchaseOrderInvoiceService;
@@ -21,13 +19,9 @@ import com.axelor.apps.supplychain.service.config.SupplyChainConfigService;
 import com.axelor.exception.AxelorException;
 import com.axelor.inject.Beans;
 import com.google.inject.Inject;
-import com.google.inject.persist.Transactional;
-
 import java.math.BigDecimal;
-import java.util.Map;
 
-public class StockMoveInvoiceServiceImplSpecifique
-    extends ProjectStockMoveInvoiceServiceImpl {
+public class StockMoveInvoiceServiceImplSpecifique extends ProjectStockMoveInvoiceServiceImpl {
 
   @Inject
   public StockMoveInvoiceServiceImplSpecifique(
@@ -67,14 +61,15 @@ public class StockMoveInvoiceServiceImplSpecifique
       invoiceLine.setPriceDiscounted(stockMoveLine.getPriceDiscounted());
 
     InvoiceLineService invoiceLineService = Beans.get(InvoiceLineService.class);
-    invoiceLine.setPriceDiscounted(invoiceLineService.computeDiscount(invoiceLine,false));
-    
+    invoiceLine.setPriceDiscounted(invoiceLineService.computeDiscount(invoiceLine, false));
+
     BigDecimal taxRate = BigDecimal.ZERO;
     if (invoiceLine.getTaxLine() != null) {
       taxRate = invoiceLine.getTaxLine().getValue();
     }
 
-    BigDecimal exTaxTotal = InvoiceLineManagement.computeAmount(invoiceLine.getQty(), invoiceLine.getPriceDiscounted());    
+    BigDecimal exTaxTotal =
+        InvoiceLineManagement.computeAmount(invoiceLine.getQty(), invoiceLine.getPriceDiscounted());
     BigDecimal inTaxTotal = exTaxTotal.add(exTaxTotal.multiply(taxRate));
     invoiceLine.setExTaxTotal(exTaxTotal);
     invoiceLine.setInTaxTotal(inTaxTotal);
